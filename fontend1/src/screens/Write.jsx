27f55def145e8abdd8fc96e69a74355d168a435e
Sounak Roy <135100128+sonroy-glitch@users.noreply.github.com>
2024-05-29 13:25:32 -0400

    Update Write.jsx

--- fontend1/src/screens/Write.jsx
+++ fontend1/src/screens/Write.jsx
@@ -47,6 +47,7 @@ const Write = () => {
       if(add.status===200){
         setHolder("Blog added")
         setBlog(add.data)
+        localStorage.setItem("blog",JSON.stringify(update.data))
     ref.current.complete()
 
        navigate("/user/api/blog")
